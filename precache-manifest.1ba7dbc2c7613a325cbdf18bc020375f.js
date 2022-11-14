6b376c7451adb3dbbe2d96a51b5b5231126b9060
Tushar <tusharnagar83@gmail.com>
2022-11-14 23:49:18 +0530

    Updates

---
+++ precache-manifest.1ba7dbc2c7613a325cbdf18bc020375f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91abcbd45ebb9bdd1470",
+    "url": "./static/css/main.f226d893.chunk.css"
+  },
+  {
+    "revision": "91abcbd45ebb9bdd1470",
+    "url": "./static/js/main.91abcbd4.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "./static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "6d0d1a021f00723516fe",
+    "url": "./static/js/2.6d0d1a02.chunk.js"
+  },
+  {
+    "revision": "e0fc40820a9c1e69c239",
+    "url": "./static/js/runtime~main.e0fc4082.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "./static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "dfff120c01ad164f977761f38f45d147",
+    "url": "./index.html"
+  }
+];
