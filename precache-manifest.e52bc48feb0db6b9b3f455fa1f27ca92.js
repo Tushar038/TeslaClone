63055bf87dd8920d78500f359aa20edb6dbf84e8
Tushar <tusharnagar83@gmail.com>
2022-11-14 23:28:41 +0530

    Updates

---
+++ precache-manifest.e52bc48feb0db6b9b3f455fa1f27ca92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91abcbd45ebb9bdd1470",
+    "url": "/TeslaClone/static/css/main.f226d893.chunk.css"
+  },
+  {
+    "revision": "91abcbd45ebb9bdd1470",
+    "url": "/TeslaClone/static/js/main.91abcbd4.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/TeslaClone/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "cee35179c108401f75a7",
+    "url": "/TeslaClone/static/js/2.cee35179.chunk.js"
+  },
+  {
+    "revision": "495d2d3195d2b5c55e1d",
+    "url": "/TeslaClone/static/js/runtime~main.495d2d31.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/TeslaClone/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "05ea5ef5777208b0baf76ff89967fdc8",
+    "url": "/TeslaClone/index.html"
+  }
+];
